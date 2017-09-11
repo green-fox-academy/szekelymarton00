a7ccf006ab38e58c492db95c7ec3fb9153ae80d7
Marci <szekelymarton00@gmail.com>
2017-09-11 15:53:20 +0200

    VariableMutation almost done

--- week-02/day_1/src/VariableMutation.java
+++ week-02/day_1/src/VariableMutation.java
@@ -3,13 +3,67 @@ public class VariableMutation {
         int a = 3;
         a += 10;
         System.out.println(a);
+        ////////////////////////////////////////////////////////////////
         int b = 100;
         b -= 7;
         System.out.println(b);
+        ////////////////////////////////////////////////////////////////
         int c = 44;
         c *= 2;
         System.out.println(c);
+        ////////////////////////////////////////////////////////////////
         int d = 125;
+        d /= 5;
+        System.out.println(d);
+        ////////////////////////////////////////////////////////////////
+        int e = 8;
+        e *= e * e;
+        System.out.println(e);
+        ////////////////////////////////////////////////////////////////
+        int f1 = 123;
+        int f2 = 345;
+
+        boolean bigger = true;
+        {
+            if (f1 > f2) {
+                System.out.println("F1 is bigger than F2");
+            } else System.out.println("F1 is smaller than F2");
+        }
+        ////////////////////////////////////////////////////////////////
+        int g1 = 350;
+        int g2 = 200;
+
+        boolean doublebigger = (g2 * 2) > g1;
+        System.out.println(doublebigger);
+
+        ////////////////////////////////////////////////////////////////
+        int h = 135798745;
+
+        boolean divisor = h / 11 >= 0;
+        System.out.println(divisor);
+
+        ////////////////////////////////////////////////////////////////
+
+        int i1 = 10;
+        int i2 = 3;
+
+        boolean gettingtired = i1 > i2*i2 && i1 < i2*i2*i2;
+        System.out.println(gettingtired);
+        ////////////////////////////////////////////////////////////////
+
+        int j = 1521;
+
+        boolean reallytired = (j % 3) == 0 || (j % 5) == 0;
+        System.out.println(reallytired);
+        ////////////////////////////////////////////////////////////////
+
+        String k = new String[] {"Apple", "Apple", "Apple", "Apple"};
+
+        System.out.println(k);
 
     }
-}
+
+        }
+
+
+
