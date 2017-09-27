6451e9229aa9f028d5130455ec12e22cbdb7993e
Marci <szekelymarton00@gmail.com>
2017-09-27 11:11:44 +0200

    first exercise done

---
+++ week-04/day_3/test/ApplesTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ApplesTest {
+
+  @Test
+  public void getAppleTest() throws Exception {
+    Apples apple = new Apples();
+    assertEquals("apple", apple.getApple());
+  }
+
+}
